7c878c714bb104f8bef0f3661e0acdfe859a6492
Wynd <wyndmaster@gmail.com>
2015-02-21 17:03:48 +0200

    0.0.1 -pre.alpha-

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 package FairyTail;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance;
 
 @Mod(modid = "fairytail", name = "Fairy Tail", version = "0.0.1")
 
 public class Main 
 {
 
-	
+	@Instance("fairytail")
+	public static Main instance;
 	
 }
